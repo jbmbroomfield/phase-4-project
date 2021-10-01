64d6e6efc152c2edb5661a258c48e986960a84a7
jim <jbmbroomfield@gmail.com>
2021-10-01 15:44:03 +1000

    First commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+sample_username = 'Alice'
+sample_email = 'alice@mail.com'
+sample
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'validation tests' do
+  
+    it 'should require username' do
+    end
+
+  end
+
+end
