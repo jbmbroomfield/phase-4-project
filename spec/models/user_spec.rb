c888a970f5620e307de51d054d26ae1f4627a3db
jim <jbmbroomfield@gmail.com>
2021-10-01 15:52:21 +1000

    Passed basic User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 sample_username = 'Alice'
 sample_email = 'alice@mail.com'
-sample
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   context 'validation tests' do
-  
-    it 'should require username' do
+      it 'should require username' do
+      user = User.new(email: sample_email).save
+      expect(user).to eq(false)
     end
-
+    it 'should require email' do
+      user = User.new(username: sample_username).save
+      expect(user).to eq(false)
+    end
+    it 'should save successfully' do
+      user = User.new(username: sample_username, email: sample_email).save
+      expect(user).to eq(true)
+    end
+    it 'should default admin to false' do
+      user = User.create(username: sample_username, email: sample_email)
+      expect(user.admin).to eq(false)
+    end
   end
 
 end
