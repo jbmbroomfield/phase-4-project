1bc7ae079bc59ce119d41121095a36a30d64f975
jim <jbmbroomfield@gmail.com>
2021-10-03 00:10:11 +1000

    Removed some p statements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,10 @@ class UsersController < ApplicationController
 
     def create
         if user_params[:password] == params[:user][:password_confirmation]
-            puts "11111111111111111111111111111111111"
-            puts user_params
             @user = User.create(user_params)
-            puts "333333333333333333333333333333333"
-            puts @user
-            puts @user.username
-            puts @user.save
             session[:user_id] = @user.id
             redirect_to root_path
         else
-            puts "222222222222222222222222222222222222"
             redirect_to new_user_path
         end
     end
