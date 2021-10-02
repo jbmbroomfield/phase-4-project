24b72a6c90a21c9f9e54c8b549357c39734a097f
jim <jbmbroomfield@gmail.com>
2021-10-02 20:12:23 +1000

    Create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_100157) do
+ActiveRecord::Schema.define(version: 2021_10_02_101153) do
 
   create_table "facebook_authentications", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_10_02_100157) do
   create_table "password_authentications", force: :cascade do |t|
     t.integer "user_id"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "thread_id"
+    t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
