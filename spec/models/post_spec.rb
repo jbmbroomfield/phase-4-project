cc5986839ee64da708a7ff90672030b6d5a31d50
jim <jbmbroomfield@gmail.com>
2021-10-02 20:16:56 +1000

    Wrote Post validation tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
+user = User.create(username: 'Alice', email: 'alice@mail.com')
+section = Section.create(title: 'Section')
+thread = ForumThread.create(section: section, title: 'Thread')
+
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'validation tests' do
+    it 'should require user' do
+      post = Post.new(thread: thread, text: text).save
+      expect(post).to eq(false)
+    end
+    it 'should require thread' do
+      post = Post.new(user: user, text: text).save
+      expect(post).to eq(false)
+    end
+    it 'should require text' do
+      post = Post.new(user: user, thread: thread).save
+      expect(post).to eq(false)
+    end
+    it 'should save successfully' do
+      post = Post.new(user: user, thread: thread, text: text).save
+      expect(post).to eq(true)
+    end
+  end
+
 end
