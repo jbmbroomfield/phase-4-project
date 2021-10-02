5917f275169ff1b0eb74444fa39e48e84b6cda51
jim <jbmbroomfield@gmail.com>
2021-10-02 19:57:41 +1000

    Created Section model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_050024) do
+ActiveRecord::Schema.define(version: 2021_10_02_095700) do
 
   create_table "facebook_authentications", force: :cascade do |t|
     t.integer "user_id"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2021_10_02_050024) do
   create_table "password_authentications", force: :cascade do |t|
     t.integer "user_id"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
