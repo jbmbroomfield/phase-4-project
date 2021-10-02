8ff9ce1ffa67f7b329d91d441b45a1be1359d757
jim <jbmbroomfield@gmail.com>
2021-10-02 20:11:10 +1000

    Passed ForumThread validation tests

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,2 +1,9 @@
 class ForumThread < ApplicationRecord
+
+    belongs_to :section
+
+    validates :section, presence: true
+    validates :title, presence: true
+    validates :title, length: { maximum: 32 }
+
 end
