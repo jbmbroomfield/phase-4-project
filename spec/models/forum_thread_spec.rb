aadd3a0bd0d3dad6e441ba02403a04a408dbf423
jim <jbmbroomfield@gmail.com>
2021-10-02 20:09:04 +1000

    Wrote ForumThread validation tests

--- spec/models/forum_thread_spec.rb
+++ spec/models/forum_thread_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
+section = Section.create(title: 'New Section')
+title = 'New Thread'
+too_long = 'New Threadddddddddddddddddddddddddd'
+
 RSpec.describe ForumThread, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'validation tests' do
+    it 'should require section' do
+      thread = ForumThread.new(title: title).save
+      expect(thread).to eq(false)
+    end
+    it 'should require title' do
+      thread = ForumThread.new(section: section).save
+      expect(thread).to eq(false)
+    end
+    it 'should require title to be no more than 32 characters' do
+      thread = ForumThread.new(section: section, title: too_long)
+    end
+    it 'should save successfully' do
+      thread = ForumThread.new(section: section, title: title).save
+    end
+  end
+
 end
