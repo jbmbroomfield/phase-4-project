eaf7449424ced532db66aa9bd33412dfd9b8f01b
jim <jbmbroomfield@gmail.com>
2021-10-04 10:26:49 +1100

    Created User index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/auth/facebook/callback' => 'sessions#facebook_create'
   get '/auth/facebook' => 'sessions#facebook_create'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :index, :show]
   resources :sections, only: [:new, :create, :show] do
     resources :forum_threads, as: :threads, path: 'threads', only: [:new, :create, :show] do
       resources :posts, only: [:new, :create]
