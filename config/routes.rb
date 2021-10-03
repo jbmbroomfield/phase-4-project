2226fb2abd1a32a02b0355a76f44635c52683bb9
jim <jbmbroomfield@gmail.com>
2021-10-03 17:04:25 +1100

    Nested Threads in Sections

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get '/auth/facebook' => 'sessions#facebook_create'
 
   resources :users, only: [:new, :create]
-  resources :sections, only: [:new, :create, :show]
-  resources :forum_threads, as: :threads, path: 'threads', only: [:new, :create, :show]
+  resources :sections, only: [:new, :create, :show] do
+    resources :forum_threads, as: :threads, path: 'threads', only: [:new, :create, :show]
+  end
 
 end
