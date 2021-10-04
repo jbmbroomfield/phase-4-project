205e4d22559b5916fec9b6bf1f13641b6dd48256
jim <jbmbroomfield@gmail.com>
2021-10-04 20:50:32 +1100

    Added New Section link for admins to Section index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
     
     before_action :get_current_user
 
+    def current_admin
+        @current_user && @current_user.admin
+    end
+
     private
 
     def get_current_user
         @current_user = User.find_by(id: session[:user_id])
+        @current_admin = @current_user && @current_user.admin
     end
 
     def require_login
