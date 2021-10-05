f8d6ed35ce579fe8bb11566bc0a2fb1cc8309612
jim <jbmbroomfield@gmail.com>
2021-10-05 16:38:13 +1100

    Renamed post css classes to row classes and refactored into application helped

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def row_class(index, total)
+        parity = index % 2 == 0 ? ' row-even' : ' row-odd'
+        first = index == 0 ? ' row-first' : ''
+        last = index == total - 1 ? ' row-last' : ''
+        "row#{parity}#{first}#{last}"
+    end
+
 end
