ba238db08224613956ca62e03d81357f68525cb9
jim <jbmbroomfield@gmail.com>
2021-10-05 16:54:42 +1100

    Added row styling to sections in home page

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,12 +2,24 @@ class Section < ApplicationRecord
 
     has_many :threads, class_name: 'ForumThread'
     has_many :posts, through: :threads
-    has_many :users, through: :posts
+    has_many :users, -> { distinct }, through: :posts
 
     validates :title, presence: true
 
     def to_s
         self.title
+    end
+
+    def threads_count
+        threads.count
+    end
+
+    def posts_count
+        posts.count
+    end
+
+    def users_count
+        users.count
     end
 
 end
